013156709da02668900a65290a89518f9a2492f6
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-08 11:53:30 -0300

    Adding mapState

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
 
-        <h2 class="font-weight-light">Contador: {{ $store.state.contador }}</h2>
+        <h2 class="font-weight-light">Contador: {{ contador }}</h2>
         <button class="btn btn-lg btn-danger mr-1" @click="decrementar">-</button>
         <button class="btn btn-lg btn-success" @click="incrementar">+</button>
     </div>
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
+    // computed: mapState(['contador']),
+    computed: mapState({
+        //contador: state => state.contador
+        contador: 'contador'
+    }),
     methods: {
         decrementar() {
             this.$store.state.contador--
