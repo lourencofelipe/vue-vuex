56763f66c695fda708948dc5d6790806c92ba5dc
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-08 16:21:26 -0300

    Adding getters like methods

--- src/store/index.js
+++ src/store/index.js
@@ -15,7 +15,8 @@ export default new Vuex.Store({
   getters: {
     tarefasConcluidas: (state) => state.tarefas.filter(t => t.concluido),
     tarefasAFazer: (state) => state.tarefas.filter(t => !t.concluido),
-    totalDeTarefasConcluidas: (state, getters) => getters.tarefasConcluidas.length
+    totalDeTarefasConcluidas: (state, getters) => getters.tarefasConcluidas.length,
+    buscarTarefasPorId: state => id => state.tarefas.find(t => t.id === id)
   },
   mutations: {
   },
