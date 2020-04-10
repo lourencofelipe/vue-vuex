42c285cc313e77898266954e4d351222bf99d53a
lourencofelipe <felipe.pucsc@gmail.com>
2020-04-10 18:54:14 -0300

    Adding mutations

--- src/resources/tarefas/_store/mutation-types.js
+++ src/resources/tarefas/_store/mutation-types.js
@@ -1,2 +1,6 @@
+
+export const CRIAR_TAREFA = 'CRIAR_TAREFA'
+export const EDITAR_TAREFA = 'EDITAR_TAREFA'
 export const LISTAR_TAREFAS = 'LISTAR_TAREFAS'
+export const DELETAR_TAREFA = 'DELETAR_TAREFA'
 
